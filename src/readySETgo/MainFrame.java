972fad9cba5c20cfa8b175508d978245b81fb413
BenjaminMarshall <marshallbenjamine@gmail.com>
2016-11-03 17:34:27 -0400

    Fixed merge conflicts

--- src/readySETgo/MainFrame.java
+++ src/readySETgo/MainFrame.java
@@ -9,52 +9,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
-<<<<<<< HEAD
 import panels.StagePanel;
-
-public class MainFrame extends JFrame{
-
-	public MainFrame(int width, int height){
-		super();
-		setSize(width, height);
-		
-		fill();
-		createMenuBar();
-		
-		setLocationRelativeTo(null);
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-	}
-	
-	private void fill(){
-		JPanel panel1 = new MockColorPanel(Color.RED);
-		JPanel panel2 = new StagePanel();
-		JPanel panel3 = new MockColorPanel(Color.GREEN);
-		
-		setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		
-		c.gridx = 0;
-		c.gridy = 0;
-		c.weightx = .2;
-		c.weighty = 1;	
-		c.fill = c.BOTH;
-		add(panel1, c);
-		
-		c.gridx = 1;
-		c.weightx = .6;
-		add(panel2, c);
-		
-		c.gridx = 2;
-		c.weightx = .2;
-		add(panel3, c);
-	}
-	
-	private void createMenuBar(){
-		JMenuBar menuBar = new JMenuBar();
-		MenuItemFactory mf = new MenuItemFactory();
-=======
 import readySETgo.toolbar.FileMenu;
 import readySETgo.toolbar.ToolsMenu;
 
@@ -75,9 +30,9 @@ public class MainFrame extends JFrame{
     }
     
     private void fill(){
-        JPanel panel1 = new MockColorPanel(Color.RED);
-        JPanel panel2 = new MockColorPanel(Color.BLUE);
-        JPanel panel3 = new MockColorPanel(Color.GREEN);
+        JPanel panel1 = new StagePanel();
+        JPanel panel2 = new StagePanel();
+        JPanel panel3 = new StagePanel();
         
         setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
@@ -105,7 +60,6 @@ public class MainFrame extends JFrame{
         // Create menus
         JMenu fileMenu = new FileMenu();
         JMenu toolsMenu = new ToolsMenu();
->>>>>>> jason-develop
 
         // Fill Menus   
         menuBar.add(fileMenu);
