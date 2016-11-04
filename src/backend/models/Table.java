0f53a66392e7b66d42d233ef0a1e8ad3bf8e2b9d
Ksenia Belikova <ksenia88belikova@gmail.com>
2016-11-03 21:57:32 -0400

    add Chair, Poduim and Table classes

---
+++ src/backend/models/Table.java
@@ -0,0 +1,10 @@
+package backend.models;
+
+import java.io.Serializable;
+
+/**
+ * @author Ksenia Belikova
+ * @version 11/3/2016.
+ */
+public class Table extends Asset implements Serializable{
+}
