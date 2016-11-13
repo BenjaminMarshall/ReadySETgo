7614c320acb6e8ae97e1c323c7323c8225a6400e
jason <jason>
2016-11-13 18:18:56 -0500

    Enable scrolling for left/right panels

--- src/readySETgo/panels/FlyRailPanel.java
+++ src/readySETgo/panels/FlyRailPanel.java
@@ -1,11 +1,16 @@
 package readySETgo.panels;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 import backend.FileManager;
 import backend.models.FlyRail;
@@ -21,19 +26,42 @@ public class FlyRailPanel extends JPanel{
 			JButton railToggle = new JButton((f.isFlownIn() ? "Fly Out" : "Fly In"));
 			this.add(railTitle);
 			this.add(railToggle);
+			this.setPreferredSize(new Dimension(200, 50));
 		}
 	}
 
 	public FlyRailPanel() {
 		super();
-		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		this.loadFlyRails();
+	
+		JPanel contentPanel = this.loadFlyRails();
+	
+		GridBagLayout gc = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		this.setLayout(gc);
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 1;
+		c.weighty = 1;
+		c.fill = c.BOTH;
+		
+		JScrollPane scrollPane = new JScrollPane(contentPanel, 
+				   ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,  
+				   ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		
+		this.add(scrollPane, c);
 	}
 
-	public void loadFlyRails(){
+	public JPanel loadFlyRails() {
+		
+		JPanel ret = new JPanel();
+		ret.setLayout(new BoxLayout(ret, BoxLayout.PAGE_AXIS));
+		
 		ArrayList<FlyRail> rails = FileManager.getListOfFlyRails();
 		this.removeAll();
-		for (FlyRail f : rails) { this.add(new SingleRailPanel(f)); }
+		for (FlyRail f : rails) { ret.add(new SingleRailPanel(f)); }
+		
+		return ret;
 	}
 
 }
