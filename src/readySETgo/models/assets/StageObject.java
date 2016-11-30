9a0178b7337e450e3b8a11b7ffc2a7196329dccf
BenjaminMarshall <marshallbenjamine@gmail.com>
2016-11-30 16:26:58 -0500

    Transparency for non-default objects

--- src/readySETgo/models/assets/StageObject.java
+++ src/readySETgo/models/assets/StageObject.java
@@ -44,15 +44,16 @@ public class StageObject extends Asset {
 	@Override
 	public void draw(Graphics g, double x, double y) {
 		g.setColor(Color.WHITE);
-		g.fillRect((int) x, (int) y, (int) getPhysicalWidth(), (int) getPhysicalLength());
+		
 		if(image == null){
 			g.setColor(Color.BLACK);
-			g.fillRect((int) x, (int) y, (int) getPhysicalWidth(), (int) getPhysicalLength());
+			g.fillRect((int) x, (int) y, (int) getPhysicalWidth(), (int) getPhysicalLength());	
 		} else if (!defaultPic){
 			g.drawImage(image, (int) x, (int) y, (int) getPhysicalWidth(), (int) getPhysicalLength(), null);
 		} else {
 			g.setColor(Color.BLACK);
 			g.drawRect((int) x, (int) y, (int) getPhysicalWidth(), (int) getPhysicalLength());
+			g.fillRect((int) x, (int) y, (int) getPhysicalWidth(), (int) getPhysicalLength());
 			g.drawImage(image, (int) x, (int) y, (int) getPhysicalWidth(), (int) getPhysicalLength(), null);
 		}
 		
