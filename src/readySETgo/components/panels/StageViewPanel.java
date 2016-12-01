60ad9a39422f166bdb997336a0fa0f55386df457
BenjaminMarshall <marshallbenjamine@gmail.com>
2016-12-01 00:23:43 -0500

    Delete Object on drag out

--- src/readySETgo/components/panels/StageViewPanel.java
+++ src/readySETgo/components/panels/StageViewPanel.java
@@ -1,25 +1,34 @@
 package readySETgo.components.panels;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JViewport;
 
+import readySETgo.managers.ComponentManager;
+
 public class StageViewPanel extends JPanel {
 	private StagePanel stagePanel;
+	private JScrollPane scroll;
 	
 	public StageViewPanel(StagePanel sp){
 		//TODO Create viewport to stagePanel
 		super(new GridLayout(1,1));
 		
+		ComponentManager.registerComp("StageViewPanel", this);
 		JViewport port = new JViewport();
-		JScrollPane scroll = new JScrollPane();
+		scroll = new JScrollPane();
 		port.add(sp);
 		scroll.setViewport(port);
 		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); 
 		add(scroll);
+	}
+
+	public JScrollPane getScrollPane() {
+		return scroll;
 	}
 	
 	
